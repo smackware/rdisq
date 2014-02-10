3f00f77d5714ba4bd903c849c3df704d997da56b
Lital Natan <smackware@shemer-laptop.(none)>
2014-02-10 12:15:31 +0200

    Pre and Post changes + added processtime to the response
    * The Result object will also include .processtime and .duration (processtime + networktime + the rest)
    * Pre and Post will accept queue_name as an argument

--- rdisq/__init__.py
+++ rdisq/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import time
 import uuid
 import redis
 
@@ -15,6 +16,9 @@ redis_pool = redis.ConnectionPool(host='localhost', port=6379, db=5)
 TASK_ID_ATTR = "task_id"
 ARGS_ATTR = "args"
 KWARGS_ATTR = "kwargs"
+
+RESULT_ATTR = "result"
+PROCESSTIME_ATTR = "processtime"
 
 EXPORTED_METHOD_PREFIX = "q_"
 
@@ -41,10 +45,16 @@ class Result(object):
     _task_id = None
     consumer = None
     response = None
+    processtime = None
+    totaltime = None
+    _start = None
+   
 
     def __init__(self, task_id, consumer):
         self._task_id = task_id
         self.consumer = consumer
+	self._start = time.time()
+
 
     # This method is deprecated
     def peek(self):
@@ -54,15 +64,17 @@ class Result(object):
         redis_con = self.consumer.queue_config.get_redis()
         return redis_con.exists(self._task_id)
 
-    def wait(self, timeout=5):
+    def wait(self, timeout=2):
         redis_con = self.consumer.queue_config.get_redis()
         redis_response = redis_con.brpop(self._task_id, timeout=timeout) # can be tuple of (queue_name, string) or None
         if redis_response is None:
             raise ResultTimeout(self._task_id)
         queue_name, response = redis_response
+        self.totaltime = time.time() - self._start
         self.response = decode(response)
+        self.processtime = self.response[PROCESSTIME_ATTR]
         redis_con.delete(self._task_id)
-        return self.response
+        return self.response[RESULT_ATTR]
 
 class Rdisq(object):
     queue_config = None
@@ -92,7 +104,13 @@ class Rdisq(object):
     @staticmethod
     def __get_sync_method(parent, queue_name):
         def c(*args, **kwargs):
-            return parent.send(queue_name, *args, **kwargs).wait()
+            last_exception = None
+            for i in xrange(0,3):
+                try:
+                    return parent.send(queue_name, *args, **kwargs).wait()
+                except ResultTimeout as e:
+                    last_exception = e
+            raise last_exception
         return c 
 
     def send(self, queue_name, *args, **kwargs):
@@ -109,11 +127,11 @@ class Rdisq(object):
     def get_queue_name(self, method_name):
         return self.queue_config.get_name() + "_" + method_name
 
-    def pre(self):
+    def pre(self, queue_name):
         """Performs after something was found in the queue"""
         pass
 
-    def post(self):
+    def post(self, queue_name):
         """Performs after a queue fetch and process"""
         pass
 
@@ -126,20 +144,26 @@ class Rdisq(object):
         """
         redis_con = self.queue_config.get_redis()
         redis_result = redis_con.brpop(self.__queue_to_callable.keys(), timeout=timeout)
-        self.pre()
         if redis_result is None: # Timeout
             return
         queue_name, data_string = redis_result
+        self.pre(queue_name)
         call = self.__queue_to_callable[queue_name]
         task_data = decode(data_string)
         task_id = task_data[TASK_ID_ATTR]
         args    = task_data[ARGS_ATTR]
         kwargs  = task_data[KWARGS_ATTR]
-        result_data = call(*args, **kwargs)
-        result_string = encode(result_data)
-        redis_con.lpush(task_id, result_string)
-        redis_con.expire(task_id, 20)
-        self.post()
+        start = time.time()
+        result = call(*args, **kwargs)
+	duration = time.time() - start
+        response = {
+            RESULT_ATTR: result,
+            PROCESSTIME_ATTR: duration,
+	}
+        response_string = encode(response)
+        redis_con.lpush(task_id, response_string)
+        redis_con.expire(task_id, 10)
+        self.post(queue_name)
         
     def process(self):
         while self.__go:
