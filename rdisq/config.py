7f8888154d1ac086ffd29e41c35b0dc445d8957b
Lital Natan <litaln@eatwith.com>
2014-02-13 10:34:33 +0200

    Fixed critical issue in PoolQueueConfig - not calling super of constructor

--- rdisq/config.py
+++ rdisq/config.py
@@ -23,6 +23,7 @@ class SimpleQueueConfig(AbstractQueueConfig):
 
 class PoolQueueConfig(SimpleQueueConfig):
     def __init__(self, name, redis_host='localhost', redis_port=6379):
+	SimpleQueueConfig.__init__(self, name)
 	self.REDIS_POOL = redis.ConnectionPool(host=redis_host, port=redis_port)
 
     def get_redis(self):
